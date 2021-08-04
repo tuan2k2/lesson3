4cad723402e719fa712e41d6bc12eed3ad7fef35
tuan2k2 <88386275+tuan2k2@users.noreply.github.com>
2021-08-04 22:29:03 +0700

    add lesson3.java file

--- src/com/basic/lesson3/lesson3.java
+++ src/com/basic/lesson3/lesson3.java
@@ -1,7 +1,18 @@
 package com.basic.lesson3;
 public class lesson3 {
     public static void main(String[] args) {
-        System.out.println("hellowordln");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
 
     }
 }
