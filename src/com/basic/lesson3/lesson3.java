6348e948f92cea83fdee13b8a568b94b5f11ed5d
tuan2k2 <88386275+tuan2k2@users.noreply.github.com>
2021-08-05 00:20:21 +0700

    add lesson3.java file

--- src/com/basic/lesson3/lesson3.java
+++ src/com/basic/lesson3/lesson3.java
@@ -1,18 +1,22 @@
 package com.basic.lesson3;
+
+import java.util.Scanner;
+
 public class lesson3 {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-
+        Scanner Name = new Scanner(System.in);
+        String Ten = Name.next();
+        // lesson1
+        System.out.println("where the character a is: " +Ten.indexOf("a") + " and " + Ten.lastIndexOf("a"));
+        //lesson2
+        System.out.println(" split string: \n"+Ten.substring(0,3) + "\n" +Ten.substring(3,6)+"\n" +Ten.substring(6,8)+"\n" +Ten.substring(8,12));
+        //lesson3
+        System.out.println("string concatenation: \n " +Ten.concat("").concat("").concat("").concat(""));
+        //lesson4
+        System.out.println("Words to get: \n" +Ten.substring(6,12));
+        //lesson5
+        System.out.println("sentence after replacing: \n" +Ten.replace("toiyeudanang" , "toiyeuhanoi"));
+        //lesson6
+        System.out.println("this sentence has a empty character: \n "  +Ten.isEmpty());
     }
 }
